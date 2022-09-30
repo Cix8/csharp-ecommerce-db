be1173803f570e9f05817d904a48c4a84cae2973
Cix8 <francesco.natal_fn@libero.it>
2022-09-30 15:16:03 +0200

    Aggiungere i file di progetto.

---
+++ E_commerce.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+public class E_commerce : DbContext
+{
+    private string connString = "Data Source=localhost;Initial Catalog = e_commerce_db; Integrated Security = True";
+    DbSet<Order> Orders { get; set; }
+    DbSet<Product> Products { get; set; }
+    DbSet<Payment> Payments { get; set; }
+    DbSet<Customer> Customers { get; set; }
+    DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(this.connString);
+    }
+}
